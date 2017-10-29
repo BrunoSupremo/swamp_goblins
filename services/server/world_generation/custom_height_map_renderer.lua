ebdbd1460da3c3963d8f2988b99e57e319243a61
Bruno <bruno_mussoi@hotmail.com>
2017-10-29 13:05:35 -0200

    Candles, window, fog and biome changes

--- services/server/world_generation/custom_height_map_renderer.lua
+++ services/server/world_generation/custom_height_map_renderer.lua
@@ -4,57 +4,148 @@ local Point3 = _radiant.csg.Point3
 local CustomHeightMapRenderer = class()
 
 function CustomHeightMapRenderer:_add_foothills_to_region(region3, rect, height)
-   local mod_name = stonehearth.world_generation:get_biome_alias()
-   --log:error('nome %s', mod_name)
-   --mod_name is the mod that has the current biome
-   local colon_pos = string.find (mod_name, ":", 1, true) or -1
-   mod_name = "_add_foothills_to_region_" .. string.sub (mod_name, 1, colon_pos-1)
-   if self[mod_name]~=nil then
-      self[mod_name](self,region3,rect,height)
-   else
-      self:_add_foothills_to_region_original(region3, rect, height)
-   end
+	local mod_name = stonehearth.world_generation:get_biome_alias()
+	--log:error('nome %s', mod_name)
+	--mod_name is the mod that has the current biome
+	local colon_pos = string.find (mod_name, ":", 1, true) or -1
+	mod_name = "_add_foothills_to_region_" .. string.sub (mod_name, 1, colon_pos-1)
+	if self[mod_name]~=nil then
+		self[mod_name](self,region3,rect,height)
+	else
+		self:_add_foothills_to_region_original(region3, rect, height)
+	end
 end
 
 function CustomHeightMapRenderer:_add_foothills_to_region_original(region3, rect, height)
-   local terrain_info = self._biome:get_terrain_info()
-   local foothills_step_size = terrain_info.foothills.step_size
-   local plains_max_height = terrain_info.plains.height_max
+	local terrain_info = self._biome:get_terrain_info()
+	local foothills_step_size = terrain_info.foothills.step_size
+	local plains_max_height = terrain_info.plains.height_max
 
-   local has_grass = height % foothills_step_size == 0
-   local soil_top = has_grass and height-1 or height
+	local has_grass = height % foothills_step_size == 0
+	local soil_top = has_grass and height-1 or height
 
-   self:_add_soil_strata_to_region(region3, Cube3(
-         Point3(rect.min.x, 0,        rect.min.y),
-         Point3(rect.max.x, soil_top, rect.max.y)
-      ))
+	self:_add_soil_strata_to_region(region3, Cube3(
+			Point3(rect.min.x, 0,		  rect.min.y),
+			Point3(rect.max.x, soil_top, rect.max.y)
+		))
 
-   if has_grass then
-      local material = self._block_types.grass_hills
-      if material == nil then
-         material = self._block_types.grass
-      end
-      region3:add_unique_cube(Cube3(
-            Point3(rect.min.x, soil_top, rect.min.y),
-            Point3(rect.max.x, height,   rect.max.y),
-            material
-         ))
-   end
+	if has_grass then
+		local material = self._block_types.grass_hills
+		if material == nil then
+			material = self._block_types.grass
+		end
+		region3:add_unique_cube(Cube3(
+				Point3(rect.min.x, soil_top, rect.min.y),
+				Point3(rect.max.x, height,	rect.max.y),
+				material
+			))
+	end
 end
 
 function CustomHeightMapRenderer:_add_foothills_to_region_swamp_goblins(region3, rect, height)
-   self:_add_soil_strata_to_region(region3, Cube3(
-      Point3(rect.min.x, 0,        rect.min.y),
-      Point3(rect.max.x, height-1, rect.max.y)
-   ))
+	self:_add_soil_strata_to_region(region3, Cube3(
+		Point3(rect.min.x, 0,		  rect.min.y),
+		Point3(rect.max.x, height-1, rect.max.y)
+	))
 
-   local material = self._block_types.grass_hills or self._block_types.grass
+	local material = self._block_types.grass_hills or self._block_types.grass
 
-   region3:add_unique_cube(Cube3(
-         Point3(rect.min.x, height-1, rect.min.y),
-         Point3(rect.max.x, height,   rect.max.y),
-         material
-      ))
+	region3:add_unique_cube(Cube3(
+			Point3(rect.min.x, height-1, rect.min.y),
+			Point3(rect.max.x, height,	rect.max.y),
+			material
+		))
+end
+
+function CustomHeightMapRenderer:_add_mountains_to_region(region3, rect, height)
+	local biome_name = stonehearth.world_generation:get_biome_alias()
+	local colon_position = string.find (biome_name, ":", 1, true) or -1
+	local mod_name_containing_the_biome = string.sub (biome_name, 1, colon_position-1)
+	local fn = "_add_mountains_to_region_" .. mod_name_containing_the_biome
+	if self[fn] ~= nil then
+		--found a function for the biome being used, named:
+		-- self:_add_mountains_to_region_<biome_name>(args,...)
+		self[fn](self, region3, rect, height)
+	else
+		--there is no function for this specific biome, so call a copy of the original from stonehearth
+		self:_add_mountains_to_region_original(region3, rect, height)
+	end
+end
+
+function CustomHeightMapRenderer:_add_mountains_to_region_original(region3, rect, height)
+	local rock_layers = self._rock_layers
+	local num_rock_layers = self._num_rock_layers
+	local i, block_min, block_max
+	local stop = false
+
+	block_min = 0
+
+	for i=1, num_rock_layers do
+		if (i == num_rock_layers) or (height <= rock_layers[i].max_height) then
+			block_max = height
+			stop = true
+		else
+			block_max = rock_layers[i].max_height
+		end
+
+		region3:add_unique_cube(Cube3(
+				Point3(rect.min.x, block_min, rect.min.y),
+				Point3(rect.max.x, block_max, rect.max.y),
+				rock_layers[i].terrain_tag
+			))
+
+		if stop then return end
+		block_min = block_max
+	end
+end
+
+function CustomHeightMapRenderer:_add_mountains_to_region_swamp_goblins(region3, rect, height)
+	local rock_layers = self._rock_layers
+	local num_rock_layers = self._num_rock_layers
+	local i, block_min, block_max
+	local stop = false
+
+	block_min = 0
+
+	local max_foothills_height = 35
+
+	for i=1, num_rock_layers do
+		if (i == num_rock_layers) or (height <= rock_layers[i].max_height) then
+			block_max = height
+			stop = true
+		else
+			block_max = rock_layers[i].max_height
+		end
+
+		local has_grass = stop and block_max > max_foothills_height and block_max <60
+		local rock_top = has_grass and block_max-1 or block_max
+
+		if has_grass then
+			region3:add_unique_cube(Cube3(
+				Point3(rect.min.x, block_min, rect.min.y),
+				Point3(rect.max.x, rock_top, rect.max.y),
+				rock_layers[8].terrain_tag
+			))
+		else
+			region3:add_unique_cube(Cube3(
+				Point3(rect.min.x, block_min, rect.min.y),
+				Point3(rect.max.x, rock_top, rect.max.y),
+				rock_layers[i].terrain_tag
+			))
+		end
+		
+		if has_grass then
+			local material = self._block_types.grass_hills or self._block_types.grass
+			region3:add_unique_cube(Cube3(
+				Point3(rect.min.x, rock_top, rect.min.y),
+				Point3(rect.max.x, block_max, rect.max.y),
+				material
+			))
+		end
+
+		if stop then return end
+		block_min = block_max
+	end
 end
 
 return CustomHeightMapRenderer
