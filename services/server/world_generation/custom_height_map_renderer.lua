b1f4f15df3d8760d2b32774863f7fe64364e2279
Bruno <bruno_mussoi@hotmail.com>
2018-04-19 01:14:12 -0300

    bug fixing

--- services/server/world_generation/custom_height_map_renderer.lua
+++ services/server/world_generation/custom_height_map_renderer.lua
@@ -4,14 +4,16 @@ local Point3 = _radiant.csg.Point3
 local CustomHeightMapRenderer = class()
 
 function CustomHeightMapRenderer:_add_foothills_to_region(region3, rect, height)
-	local mod_name = stonehearth.world_generation:get_biome_alias()
-	--log:error('nome %s', mod_name)
-	--mod_name is the mod that has the current biome
-	local colon_pos = string.find (mod_name, ":", 1, true) or -1
-	mod_name = "_add_foothills_to_region_" .. string.sub (mod_name, 1, colon_pos-1)
-	if self[mod_name]~=nil then
-		self[mod_name](self,region3,rect,height)
+	local biome_name = stonehearth.world_generation:get_biome_alias()
+	local colon_position = string.find (biome_name, ":", 1, true) or -1
+	local mod_name_containing_the_biome = string.sub (biome_name, 1, colon_position-1)
+	local fn = "_add_foothills_to_region_" .. mod_name_containing_the_biome
+	if self[fn] ~= nil then
+		--found a function for the biome being used, named:
+		-- self:_add_foothills_to_region_<biome_name>(args,...)
+		self[fn](self, region3, rect, height)
 	else
+		--there is no function for this specific biome, so call a copy of the original from stonehearth
 		self:_add_foothills_to_region_original(region3, rect, height)
 	end
 end
@@ -107,7 +109,7 @@ function CustomHeightMapRenderer:_add_mountains_to_region_swamp_goblins(region3,
 
 	block_min = 0
 
-	local max_foothills_height = 33 --hardcoded value
+	local max_foothills_height = 35 --hardcoded value
 
 	for i=1, num_rock_layers do
 		if (i == num_rock_layers) or (height <= rock_layers[i].max_height) then
@@ -117,22 +119,15 @@ function CustomHeightMapRenderer:_add_mountains_to_region_swamp_goblins(region3,
 			block_max = rock_layers[i].max_height
 		end
 
-		local has_grass = stop and block_max > max_foothills_height and block_max <58 --hardcoded value
+		local has_grass = stop and block_max > max_foothills_height and block_max <60 --hardcoded value
 		local rock_top = has_grass and block_max-1 or block_max
+		local terrain_tag = has_grass and rock_layers[8].terrain_tag or rock_layers[i].terrain_tag
 
-		if has_grass then
-			region3:add_unique_cube(Cube3(
-				Point3(rect.min.x, block_min, rect.min.y),
-				Point3(rect.max.x, rock_top, rect.max.y),
-				rock_layers[8].terrain_tag
-			))
-		else
-			region3:add_unique_cube(Cube3(
-				Point3(rect.min.x, block_min, rect.min.y),
-				Point3(rect.max.x, rock_top, rect.max.y),
-				rock_layers[i].terrain_tag
-			))
-		end
+		region3:add_unique_cube(Cube3(
+			Point3(rect.min.x, block_min, rect.min.y),
+			Point3(rect.max.x, rock_top, rect.max.y),
+			terrain_tag
+		))
 		
 		if has_grass then
 			local material = self._block_types.grass_hills or self._block_types.grass
--- services/server/world_generation/custom_landscaper.lua
+++ services/server/world_generation/custom_landscaper.lua
@@ -5,13 +5,16 @@ local CustomLandscaper = class()
 local log = radiant.log.create_logger('meu_log')
 
 function CustomLandscaper:place_features(tile_map, feature_map, place_item)
-	local mod_name = stonehearth.world_generation:get_biome_alias()
-	--mod_name is the mod that has the current biome
-	local colon_pos = string.find (mod_name, ":", 1, true) or -1
-	mod_name = "place_features_" .. string.sub (mod_name, 1, colon_pos-1)
-	if self[mod_name]~=nil then
-		self[mod_name](self,tile_map, feature_map, place_item)
+	local biome_name = stonehearth.world_generation:get_biome_alias()
+	local colon_position = string.find (biome_name, ":", 1, true) or -1
+	local mod_name_containing_the_biome = string.sub (biome_name, 1, colon_position-1)
+	local fn = "place_features_" .. mod_name_containing_the_biome
+	if self[fn] ~= nil then
+		--found a function for the biome being used, named:
+		-- self:place_features_<biome_name>(args,...)
+		self[fn](self, tile_map, feature_map, place_item)
 	else
+		--there is no function for this specific biome, so call a copy of the original from stonehearth
 		self:place_features_original(tile_map, feature_map, place_item)
 	end
 end
@@ -26,29 +29,35 @@ function CustomLandscaper:place_features_original(tile_map, feature_map, place_i
 end
 
 function CustomLandscaper:place_features_swamp_goblins(tile_map, feature_map, place_item)
-	local water_1_table = WeightedSet(self._rng)
-	for item, weight in pairs(self._landscape_info.water.spawn_objects.water_1) do
-		water_1_table:add(item,weight)
-	end
-	local water_2_table = WeightedSet(self._rng)
-	for item, weight in pairs(self._landscape_info.water.spawn_objects.water_2) do
-		water_2_table:add(item,weight)
+	local water_1_table
+	local water_2_table
+	if self._landscape_info.water.spawn_objects then
+		water_1_table = WeightedSet(self._rng)
+		water_2_table = WeightedSet(self._rng)
+		for item, weight in pairs(self._landscape_info.water.spawn_objects.water_1) do
+			water_1_table:add(item,weight)
+		end
+		for item, weight in pairs(self._landscape_info.water.spawn_objects.water_2) do
+			water_2_table:add(item,weight)
+		end
 	end
 
 	local new_feature
 	for j=1, feature_map.height do
 		for i=1, feature_map.width do
 			local feature_name = feature_map:get(i, j)
-			if feature_name == "water_1" then
-				new_feature = water_1_table:choose_random()
-				if new_feature ~= "none" then
-					feature_name = new_feature
+			if self._landscape_info.water.spawn_objects then
+				if feature_name == "water_1" then
+					new_feature = water_1_table:choose_random()
+					if new_feature ~= "none" then
+						feature_name = new_feature
+					end
 				end
-			end
-			if feature_name == "water_2" then
-				new_feature = water_2_table:choose_random()
-				if new_feature ~= "none" then
-					feature_name = new_feature
+				if feature_name == "water_2" then
+					new_feature = water_2_table:choose_random()
+					if new_feature ~= "none" then
+						feature_name = new_feature
+					end
 				end
 			end
 			self:_place_feature_swamp_goblins(feature_name, i, j, tile_map, place_item)
