028518e7088e0a195ee0eeb6d034651da60f2f1f
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2020-01-01 21:52:35 -0300

    lag free water pathfinder

--- services/server/world_generation/custom_world_generation_service.lua
+++ services/server/world_generation/custom_world_generation_service.lua
@@ -1,5 +1,6 @@
 local SwampWorldGenerationService = class()
 local Cube3 = _radiant.csg.Cube3
+local Region3 = _radiant.csg.Region3
 
 function SwampWorldGenerationService:_add_water_bodies(regions)
 	local biome_landscape_info = self._biome_generation_data:get_landscape_info()
@@ -19,7 +20,16 @@ function SwampWorldGenerationService:_add_water_bodies(regions)
 		local water_region = terrain_region:intersect_cube(water_bounds)
 		stonehearth.hydrology:create_water_body_with_region(water_region, height)
 
-		self:add_negative_pathfind_weight_in_water(water_region)
+		local water_bounds_negative = Cube3(terrain_bounds)
+		water_bounds_negative.max.y = water_bounds_negative.max.y - math.floor(water_height_delta)
+		if water_bounds_negative.min.y < 29 then
+			water_bounds_negative.min.y = 29
+		end
+
+		local water_region_negative = terrain_region:intersect_cube(water_bounds_negative)
+		for cube in water_region_negative:each_cube() do
+			self:add_negative_pathfind_weight_in_water(Region3(cube))
+		end
 	end
 end
 
@@ -37,21 +47,21 @@ function SwampWorldGenerationService:add_negative_pathfind_weight_in_water(regio
 end
 
 function SwampWorldGenerationService:select_origin_for_region(region)
-   if region:empty() then
-      return nil
-   end
+	if region:empty() then
+		return nil
+	end
 
-   local bounds = region:get_bounds()
-   local bottom_slice = Cube3(bounds)
-   bottom_slice.max.y = bottom_slice.min.y + 1
+	local bounds = region:get_bounds()
+	local bottom_slice = Cube3(bounds)
+	bottom_slice.max.y = bottom_slice.min.y + 1
 
-   -- origin needs to be at the bottom of the region, preferably towards the center
-   local footprint = region:intersect_cube(bottom_slice)
-   local centroid = footprint:get_centroid():to_closest_int()
-   -- concave shapes may have centroids outside the region
-   -- 1x1 regions will also round the centroid outside the region
-   local origin = footprint:get_closest_point(centroid)
-   return origin
+	-- origin needs to be at the bottom of the region, preferably towards the center
+	local footprint = region:intersect_cube(bottom_slice)
+	local centroid = footprint:get_centroid():to_closest_int()
+	-- concave shapes may have centroids outside the region
+	-- 1x1 regions will also round the centroid outside the region
+	local origin = footprint:get_closest_point(centroid)
+	return origin
 end
 
 function SwampWorldGenerationService:_create_water_body_internal(location, boxed_region)
@@ -66,7 +76,14 @@ function SwampWorldGenerationService:_create_water_body_internal(location, boxed
 	mms:set_modifier(0)
 	mms:set_nav_preference_modifier(-0.5)
 
-	radiant.terrain.place_entity_at_exact_location(entity, location)
+	local delayed_function = function ()
+		radiant.terrain.place_entity_at_exact_location(entity, location)
+		if self.delay_to_escape_lag then
+			self.delay_to_escape_lag:destroy()
+			self.delay_to_escape_lag = nil
+		end
+	end
+	self.delay_to_escape_lag = stonehearth.calendar:set_persistent_timer("water_negative_pathfind delay", "30m+30m", delayed_function)
 
 	self.__saved_variables:mark_changed()
 end
